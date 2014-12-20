eb027ae73064d5ca831df1e94a75bbe19abefde4
arek00 <arkadiusz.pikulski@gmail.com>
2014-12-21 00:08:21 +0100

    ...

---
+++ src/com/example/managers/Connection.java
@@ -0,0 +1,114 @@
+package com.example.managers;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Admin on 2014-12-20.
+ */
+public class Connection extends AsyncTask {
+
+    private long startDownloadTime;
+    private long finishDownloadTime;
+    private int size;
+
+    public Connection(String address)
+    {
+        doInBackground(address);
+    }
+
+
+    @Override
+    protected String doInBackground(Object... objects) {
+
+        try {
+            URL url = new URL((String) objects[0]);
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setReadTimeout(10000);
+            connection.setConnectTimeout(15000);
+            connection.setRequestMethod("GET");
+            connection.setDoInput(false);
+
+            InputStream inputStream = connection.getInputStream();
+
+            startDownloadTime = System.nanoTime();
+            connection.connect();
+            finishDownloadTime = System.nanoTime();
+            size = Connection.calculateSize(inputStream);
+
+        }
+        catch (MalformedURLException e)
+        {
+            return "MalformedUrl";
+
+        } catch (IOException e) {
+            return "IOException";
+        }
+
+        return "Complished";
+    }
+
+    public static int calculateSize(InputStream inputStream) throws IOException {
+        int size = 0;
+        while( inputStream.read() >= 0 )
+        {
+            ++size;
+        }
+
+        return size;
+    }
+
+
+    public long getDownloadTime()
+    {
+        return finishDownloadTime - startDownloadTime;
+    }
+
+    public double getDownloadTimeInSeconds()
+    {
+        return (finishDownloadTime - startDownloadTime)/1000000;
+    }
+
+    public int getDownloadSizeInBytes()
+    {
+        return size;
+    }
+
+    public double getConnectionSpeed(char unit)
+    {
+        int scalar = 1;
+        unit = Character.toLowerCase(unit);
+
+        switch(unit)
+        {
+            case 'k': scalar = (int)Math.pow(2,10); break;
+            case 'm': scalar = (int)Math.pow(2,20); break;
+            case 'g': scalar = (int)Math.pow(2,30); break;
+            default: scalar = 1; break;
+        }
+
+        return scalar * size / getDownloadTimeInSeconds();
+    }
+
+    public double getConnectionSpeed()
+    {
+        //zwraca prędkość w B/s
+        return size/getDownloadTimeInSeconds();
+    }
+
+    public String getDownloadReadableInformations()
+    {
+        String info = String.format("Pobrano %d B danych\n" +
+                                    "Średnia prędkość pobierania %f kB/s",getDownloadSizeInBytes(), getConnectionSpeed('k'));
+
+        return info;
+    }
+
+}
--- src/com/example/managers/ConnectionManagement.java
+++ src/com/example/managers/ConnectionManagement.java
@@ -1,26 +1,63 @@
 package com.example.managers;
 
 import android.app.Activity;
+import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import org.w3c.dom.Text;
 
 /**
  * Created by Admin on 2014-12-20.
  */
 public class ConnectionManagement extends Activity {
 
-    ConnectivityManager connectivityManager;
-    NetworkInfo networkInfo;
-
+    private ConnectivityManager connectivityManager;
+    private NetworkInfo networkInfo;
+    private Connection connection;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.connection);
+
+        connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        networkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
     }
 
 
+
+    public void setWifi(View view)
+    {
+        networkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+    }
+
+    public void setEthernet(View view)
+    {
+        networkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_ETHERNET);
+    }
+
+    public void setWimax(View view)
+    {
+        networkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIMAX);
+    }
+
+    public void setMMS(View view)
+    {
+        networkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE_MMS);
+    }
+
+    public void connect(View view)
+    {
+        EditText addressField = (EditText)findViewById(R.id.urlAddressField);
+        String urlAddress = addressField.getText().toString();
+        this.connection = new Connection(urlAddress);
+    }
 
 
 
